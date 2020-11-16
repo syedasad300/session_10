a1e303277a65a7fd0a0ec6a23a57c467cc4fa0a2
syed asad <smasadullah300@gmail.com>
2020-11-16 16:25:51 +0500

    Boot-camp session_10 api of covid not working

---
+++ src/Components/InfoPanel.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 1000,
+    margin: '0 auto',
+    marginTop: 50
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
+export default function InfoPanel() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+      <Grid item xs={12} sm={4}>
+          <Paper className={classes.paper} elevation={3}>Grid 1</Paper>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Paper className={classes.paper} elevation={3}>Grid 2</Paper>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Paper className={classes.paper} elevation={3}>Grid 3</Paper>
+        </Grid>
+        
+      </Grid>
+    </div>
+  );
+}
